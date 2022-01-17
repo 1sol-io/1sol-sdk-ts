69adde6960bfe1a49fd7c411b645bd52eec9b52d
gaowhen <gaowhen.com@gmail.com>
2022-01-17 12:34:35 +0800

    feat: replace fetch with axios

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,9 @@ export {
   SABER_STABLE_SWAP_NAME,
   ORCA_SWAP_PROGRAM_ID,
   RAYDIUN_V4_PROGRAM_ID,
-  ONE_MOON_SWAP_PROGRAM_ID
+  ONE_MOON_SWAP_PROGRAM_ID,
+  SAROS_SWAP_PROGRAM_ID,
+  PROVIDER_MAP
 } from './const'
 
 export { TokenInfo, TokenExtensions } from './util/token-registry'
--- types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { TokenInfo, TokenExtensions } from '../src/util/token-registry'
-export { TokenAccountInfo } from '../src/model/token'
-export { RawRoute, RawDistribution as Route } from '../src/onesolprotocol'
